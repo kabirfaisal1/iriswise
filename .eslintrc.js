06ce498e656cb32ac9f4153ef4010a2c95804e1a
kabirfaisal1 <md.kabirfaisal@outlook.com>
2024-04-29 13:51:38 -0400

    fixed lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-const { isVariableStatement } = require( "typescript" );
-
 module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
@@ -7,8 +5,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin: prettier / recommended',
-    'extends": "next/core-web-vitals'
+    'plugin:prettier/recommended',
+    'next/core-web-vitals' // Corrected the misplaced quote here
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -20,21 +18,11 @@ module.exports = {
     ],
     'react/prop-types': ['error', {
       'ignore': ['data-testid', "testid"]
-    }]
-    ,
+    }],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     indent: ['error', 2],
     camelcase: 'warn',
-    classMethodsUseThis: 'off',
-    noUnusedVars: 'warn',
-    noUnusedLocals: 'warn',
-    noImplicitReturns: 'warn',
-    UPPER_SNAKE_CASE: 'warn',
-    "import/order": ["error", {
-      "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
-      "newlines-between": "always",
-      "alphabetize": { "order": "asc", "caseInsensitive": true }
-    }],
+    'class-methods-use-this': 'off', // Corrected the rule name here
   },
 };
