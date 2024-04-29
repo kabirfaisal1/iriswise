9501a9554c68d7414ad3f021121f2d9489033945
kabirfaisal1 <md.kabirfaisal@outlook.com>
2024-04-29 15:04:48 -0400

    changed dark mode color on for the headers

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,17 @@
 const DashboardPage = () => {
 	return (
 		<div>
-			<h1>Hello, world! DashboardPage </h1>
+			<div className='md-8 space-y-4'>
+				<h1
+					className='text-2xl md:text-4xl font-bold text-center'
+					data-testid='user_dashboard_header'
+				>
+					Explore the power of AI
+				</h1>
+				<p>
+					Chat with our AI to get started. We can help you with anything you
+				</p>
+			</div>
 		</div>
 	);
 };
