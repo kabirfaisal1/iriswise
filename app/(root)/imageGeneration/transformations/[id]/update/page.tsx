f89dd63805a80aaea9db76f4859c1de610395403
kabirfaisal1 <md.kabirfaisal@outlook.com>
2024-05-06 16:19:32 -0400

    done wil file str

---
+++ app/(root)/imageGeneration/transformations/[id]/update/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const UpdateTransformationPage = () => {
+	return (
+		<>
+			<div>
+				<h1 data-testid='landing_header_title'>
+					Hello, Home! updateTransformationPage
+				</h1>
+			</div>
+		</>
+	);
+};
+export default UpdateTransformationPage;
