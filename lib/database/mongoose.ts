26acffbcbd2d55a9dd0a044931caa8e864ee7fc8
kabirfaisal1 <md.kabirfaisal@outlook.com>
2024-05-14 10:14:24 -0400

    fixed the mongoose db connection issue

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -2,7 +2,7 @@ import mongoose, { Mongoose } from 'mongoose';
 
 
 // Load environment variables
-const MONGO_URI = process.env.MONGO_URI;
+const MONGODB_URL = process.env.MONGODB_URL;
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 
 // Define the connection
@@ -18,22 +18,23 @@ let cached: MongooseConnection = ( global as any ).mongoose;
 // Check if the connection is already established
 if ( !cached )
 {
-    cached = ( global as any ).mongoose = { conn: null, promise: null };
+    cached = ( global as any ).mongoose = {
+        conn: null, promise: null
+    };
 }
 
 // Connect to the database
 export const connectToDatabase = async () =>
 {
-    if ( cached.conn )
-    {
-        return cached.conn;
-    }
+    if ( cached.conn ) return cached.conn;
 
-    if ( !MONGO_URI ) throw new Error( 'Mongo URI is missing' );
+    if ( !MONGODB_URL ) throw new Error( 'Missing MONGODB_URL' );
 
-    cached.promise = cached.promise || mongoose.connect( MONGO_URI, {
-        dbName: MONGO_DB_NAME, bufferCommands: false
-    } );
+    cached.promise =
+        cached.promise ||
+        mongoose.connect( MONGODB_URL, {
+            dbName: MONGO_DB_NAME, bufferCommands: false
+        } );
 
     cached.conn = await cached.promise;
 
