9e6ea150e5187c01271a405739373d94d766ad39
kabirfaisal1 <md.kabirfaisal@outlook.com>
2024-05-19 10:13:08 -0400

    need to add data-testid

--- components/index.ts
+++ components/index.ts
@@ -3,63 +3,64 @@ import MobileNav from '@/components/shared/mobile-sidebar';
 import Sidebar from '@/components/shared/sidebar';
 import Empty from './shared/empty';
 import Header from './shared/header';
+import Loader from './shared/loader';
 import { Button } from './ui/button';
 import { Label } from './ui/label';
 import { ModeToggle } from './ui/light_darkmode';
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
 
 
-import
-{
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from './ui/card';
-import
-{
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuPortal,
-    DropdownMenuRadioGroup,
-    DropdownMenuRadioItem,
-    DropdownMenuSeparator,
-    DropdownMenuShortcut,
-    DropdownMenuSub,
-    DropdownMenuSubContent,
-    DropdownMenuSubTrigger,
-    DropdownMenuTrigger,
-} from './ui/dropdown-menu';
-import
-{
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from './ui/table';
-import UserAvatar from './shared/userAvatar';
 import BotAvatar from './shared/botAvatar';
+import UserAvatar from './shared/userAvatar';
+import
+    {
+        Card,
+        CardContent,
+        CardDescription,
+        CardFooter,
+        CardHeader,
+        CardTitle,
+    } from './ui/card';
+import
+    {
+        DropdownMenu,
+        DropdownMenuCheckboxItem,
+        DropdownMenuContent,
+        DropdownMenuGroup,
+        DropdownMenuItem,
+        DropdownMenuLabel,
+        DropdownMenuPortal,
+        DropdownMenuRadioGroup,
+        DropdownMenuRadioItem,
+        DropdownMenuSeparator,
+        DropdownMenuShortcut,
+        DropdownMenuSub,
+        DropdownMenuSubContent,
+        DropdownMenuSubTrigger,
+        DropdownMenuTrigger,
+    } from './ui/dropdown-menu';
+import
+    {
+        Table,
+        TableBody,
+        TableCaption,
+        TableCell,
+        TableFooter,
+        TableHead,
+        TableHeader,
+        TableRow,
+    } from './ui/table';
 
 
 export
 {
-    Button, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator,
+    BotAvatar, Button, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator,
     DropdownMenuShortcut, DropdownMenuSub,
     DropdownMenuSubContent,
-    DropdownMenuSubTrigger, DropdownMenuTrigger, Empty, Header, Label, MobileNav, ModeToggle,
+    DropdownMenuSubTrigger, DropdownMenuTrigger, Empty, Header, Label, Loader, MobileNav, ModeToggle,
     Sheet,
     SheetContent,
     SheetTrigger, Sidebar, Table, TableBody, TableCaption, TableCell, TableFooter,
-    TableHead, TableHeader, TableRow, UserAvatar, BotAvatar
+    TableHead, TableHeader, TableRow, UserAvatar
 };
 
