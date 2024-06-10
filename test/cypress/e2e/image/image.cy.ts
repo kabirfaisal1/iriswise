57a023588417f9cbd51dff102018c8bfe2fa5ce5
kabirfaisal1 <md.kabirfaisal@outlook.com>
2024-06-10 15:02:32 -0400

    added extra test case for imgs

--- test/cypress/e2e/image/image.cy.ts
+++ test/cypress/e2e/image/image.cy.ts
@@ -1,4 +1,5 @@
 import ImagePO from '../../support/pageComponents/imagePC/imagePage';
+import { amountOptions } from '../../../../app/(root)/imageGeneration/constants';
 const Image = new ImagePO();
 
 describe( 'User Image', () =>
@@ -25,16 +26,43 @@ describe( 'User Image', () =>
         cy.dashboardNavigate( title );
         Image.verifyImageHeader( title, subtitle );
     } );
-    it.only( 'Generate Image', () =>
+    it( 'Generate 5 Image', () =>
     {
         const title = 'Image Generation';
         const inputValue = 'a sunset in hawaii';
+        const amountOptions = '5 Photos';
+        const resolution = '1024x1024';
         cy.sidebarNavigateScreen( title );
         Image.enterImageInput( inputValue );
-        Image.selectImageAmount( '5 Photos' );
-        Image.selectImageResolution( '1024x1024' );
+        Image.selectImageAmount( amountOptions );
+        Image.selectImageResolution( resolution );
         Image.clickImageGenerate();
         Image.verifyImageGenerated( 5 );
-
+    } );
+    it( 'Generate 3 Image', () =>
+    {
+        const title = 'Image Generation';
+        const inputValue = 'a sunset in hawaii';
+        const amountOptions = '3 Photos';
+        const resolution = '1024x1024';
+        cy.sidebarNavigateScreen( title );
+        Image.enterImageInput( inputValue );
+        Image.selectImageAmount( amountOptions );
+        Image.selectImageResolution( resolution );
+        Image.clickImageGenerate();
+        Image.verifyImageGenerated( 3 );
+    } );
+    it( 'Generate 1 Image', () =>
+    {
+        const title = 'Image Generation';
+        const inputValue = 'a sunset in hawaii';
+        const amountOptions = '1 Photos';
+        const resolution = '1024x1024';
+        cy.sidebarNavigateScreen( title );
+        Image.enterImageInput( inputValue );
+        Image.selectImageAmount( amountOptions );
+        Image.selectImageResolution( resolution );
+        Image.clickImageGenerate();
+        Image.verifyImageGenerated( 1 );
     } );
 } );
